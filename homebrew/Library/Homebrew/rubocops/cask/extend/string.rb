6536d029649a677bb4e6e98d5a30320ec081eaf7
MPit <zerk.parker@gmail.com>
2021-01-13 15:57:48 +0700

    cant control this yet

---
+++ homebrew/Library/Homebrew/rubocops/cask/extend/string.rb
@@ -0,0 +1,12 @@
+# typed: true
+# frozen_string_literal: true
+
+# Utility method extensions for String.
+class String
+  extend T::Sig
+
+  sig { returns(String) }
+  def undent
+    gsub(/^.{#{(slice(/^ +/) || '').length}}/, "")
+  end
+end
